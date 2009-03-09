05da7c1cb9daedba327599209c59c7e6e3243708
mark.nankman <mark.nankman@ae5299c4-aea4-11dd-8748-9355f1163679>
2009-03-09 21:38:05 +0000

    use Twitter4J 1.1.7

--- sign.bat
+++ sign.bat
@@ -1,11 +1,11 @@
 pack200 --repack dist\TweetBox.jar
 pack200 --repack dist\lib\JFXtras-0.3.jar
-pack200 --repack dist\lib\twitter4j-1.1.5.jar
+pack200 --repack dist\lib\twitter4j-1.1.7.jar
 
 jarsigner -keystore "http://www.xs4all.nl/~mnankman/tweetbox/keystore" -verbose -keypass tweetbox -storepass tweetbox dist\TweetBox.jar mnankman
 
 jarsigner -keystore "http://www.xs4all.nl/~mnankman/tweetbox/keystore" -verbose -keypass tweetbox -storepass tweetbox dist\lib\JFXtras-0.3.jar mnankman
 
-jarsigner -keystore "http://www.xs4all.nl/~mnankman/tweetbox/keystore" -verbose -keypass tweetbox -storepass tweetbox dist\lib\twitter4j-1.1.5.jar mnankman
+jarsigner -keystore "http://www.xs4all.nl/~mnankman/tweetbox/keystore" -verbose -keypass tweetbox -storepass tweetbox dist\lib\twitter4j-1.1.7.jar mnankman
 
 pause
