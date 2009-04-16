3772df36cfb48c1343a34d06a4320a4683a8c6ea
mark.nankman <mark.nankman@ae5299c4-aea4-11dd-8748-9355f1163679>
2009-04-16 13:15:18 +0000

    make MobypictureUtil work

--- src/tweetbox/util/MobypictureUtil.java
+++ src/tweetbox/util/MobypictureUtil.java
@@ -20,15 +20,31 @@ import java.util.logging.Logger;
 public class MobypictureUtil {
     public static final String MOBYPICTURE_API = "http://api.mobypicture.com";
     public static final String MOBYPICTURE = "http://mobypicture.com";
+    public static final String APIKEY = "tw3333tb00x";
 
     public static String getThumbUrl(String picId) {
         if (picId == null || picId.trim().length() == 0) return null;
-        return MOBYPICTURE_API + "?t={MOBYPICTURE}/?{picId}&action=getThumbUrl&s=small&k=aaaaaaa&format=plain";
+        try {
+            String encodedApiKey = java.net.URLEncoder.encode(APIKEY, "UTF-8");
+            String request = MOBYPICTURE_API + "?t=" + picId
+                    + "&action=getThumbUrl&s=small&k=" + encodedApiKey + "&format=plain";
+            System.out.println("MobypictureUtil.getThumbUrl(" + request + ")");
+            URL url = new URL(request);
+            URLConnection connection;
+            connection = url.openConnection();
+            connection.setDoInput(true);
+            BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            return input.readLine();
+        } catch (IOException ex) {
+            Logger.getLogger(UrlShrinker.class.getName()).log(Level.SEVERE, null, ex);
+            return picId;
+        }
+
     }
 
     public static String extractPicId(String url) {
         if (url.startsWith(MOBYPICTURE)) {
-            return url.substring(MOBYPICTURE.length()+1);
+            return url.substring(MOBYPICTURE.length()+2);
         }
         else {
             return null;
